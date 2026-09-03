d879b1dfbcd1cb87480d6b34779bf840fd9c6f98
FNP191 <facundonoble191@gmail.com>
2026-09-03 11:14:25 -0300

    Add files via upload

--- arbusto.gd
+++ arbusto.gd
@@ -1,11 +1,43 @@
+class_name Arbusto
 extends Node2D
 
+@export var espinas: PackedScene
+@export var area_2d: Area2D
 @export var sprite_2d: Sprite2D
 
-# Recibe una señal cuando un jugador entra.
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	# Si lo que lo toca es un Jugador lo mata
-	if body is Jugador:
-		# Llama a la funcion daño, que la deberia de tener el Jugador :)
-		body.daño()
+func disparar():
+	var target:Jugador = _find_closest_enemy()
 	
+	if not target:
+		return
+		
+	var instancia_proyectil :Espinas = espinas.instantiate()
+	
+	var nodo_padre = get_tree().current_scene
+	
+	nodo_padre.add_child(instancia_proyectil)
+	
+	instancia_proyectil.global_position = %SpawnPoint.global_position
+	
+	
+	instancia_proyectil.rotation = self.rotation
+	print("Disparando espina en: ", instancia_proyectil.global_position)
+	
+func _find_closest_enemy() -> Jugador:
+	var bodies :Array = area_2d.get_overlapping_bodies()
+	var distancia_mas_corta: float = INF
+	var target:Jugador = null
+	
+	for body in bodies:
+		if body is Jugador:
+			var dist = global_position.distance_to(body.global_position)
+			if dist < distancia_mas_corta:
+				distancia_mas_corta = dist
+				target = body
+	
+	return target
+	
+
+func _on_timer_timeout() -> void:
+	disparar()
+	%Timer.start()
